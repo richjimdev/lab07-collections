737f944204705c4fcd1617ab83096ab6ef1988ea
Richard J <richjim89@gmail.com>
2018-10-17 12:01:08 -0700

    Card class

---
+++ Collection/Collection/Classes/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection.Classes
+{
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public string Value { get; set; }
+    }
+
+    public enum Suit
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+}
