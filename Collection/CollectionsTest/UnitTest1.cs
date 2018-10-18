bda27c82d84fb0dde4f379dee5841da929497bcd
richjimdev <richjimdev@gmail.com>
2018-10-17 21:57:56 -0700

    test 2

--- Collection/CollectionsTest/UnitTest1.cs
+++ Collection/CollectionsTest/UnitTest1.cs
@@ -6,6 +6,9 @@ namespace CollectionsTest
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Testing if a card can be added
+        /// </summary>
         [Fact]
         public void AddSuccessfullyAddsCard()
         {
@@ -18,5 +21,22 @@ namespace CollectionsTest
             Assert.Equal(1, myDeck.count);
         }
 
+        /// <summary>
+        /// Testing whether a card can be removed
+        /// </summary>
+        [Fact]
+        public void RemoveRemovesACard()
+        {
+            //Arrange
+            Deck<Card> myDeck = new Deck<Card>();
+            Card myCard = new Card { Suit = Suit.Spades, Value = Value.Ace };
+            myDeck.Add(myCard);
+
+            myDeck.Remove(0);
+
+            //Assert
+            Assert.Equal(0, myDeck.count);
+        }
+        
     }
 }
