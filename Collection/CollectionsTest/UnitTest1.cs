ab141075f5bca582b7b059f8ed190c96929bfbf2
richjimdev <richjimdev@gmail.com>
2018-10-17 22:33:24 -0700

    finished tests

--- Collection/CollectionsTest/UnitTest1.cs
+++ Collection/CollectionsTest/UnitTest1.cs
@@ -37,6 +37,53 @@ namespace CollectionsTest
             //Assert
             Assert.Equal(0, myDeck.count);
         }
-        
+
+        /// <summary>
+        /// Tests if return suit will return cards only belonging to that suit
+        /// </summary>
+        /// <param name="input">Enum index of suit</param>
+        /// <param name="expected">count of cards of that suit</param>
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(1, 3)]
+        [InlineData(2, 2)]
+        [InlineData(3, 1)]
+        public void ReturnOfSuitReturnsCardsOfSameSuit(int input, int expected)
+        {
+            //Arrange
+            Deck<Card> myDeck = new Deck<Card>();
+            Card spade = new Card { Suit = Suit.Spades, Value = Value.Ace };
+            Card heart = new Card { Suit = Suit.Hearts, Value = Value.Ace };
+            Card diamond = new Card { Suit = Suit.Diamonds, Value = Value.Ace };
+            Card club = new Card { Suit = Suit.Clubs, Value = Value.Ace };
+
+
+            myDeck.Add(spade);
+            myDeck.Add(spade);
+            myDeck.Add(club);
+            myDeck.Add(club);
+            myDeck.Add(club);
+            myDeck.Add(diamond);
+            myDeck.Add(diamond);
+            myDeck.Add(heart);
+
+            //Assert
+            Assert.Equal(expected, myDeck.ReturnOfSuit((Suit)input).count);
+        }
+
+        /// <summary>
+        /// Testing that no cards return if there are none of that suit
+        /// </summary>
+        [Fact]
+        public void ReturnOfSuitReturnsZeroIfNoneFound()
+        {
+            //Arrange
+            Deck<Card> myDeck = new Deck<Card>();
+            Card myCard = new Card { Suit = Suit.Spades, Value = Value.Ace };
+            myDeck.Add(myCard);
+
+            //Assert
+            Assert.Equal(0, myDeck.ReturnOfSuit((Suit)2).count);
+        }
     }
 }
